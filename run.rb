c5d8e455ad1c10e41af6412087a26067af1475d5
Rob Murray <robmurray17@gmail.com>
2014-01-28 19:00:40 +0000

    Tabs == spaces

--- run.rb
+++ run.rb
@@ -3,17 +3,17 @@ require File.join(File.dirname(__FILE__), 'src/', 'ferver')
 
 def main
 
-	# use the first argument as the file path to serve from
-	ferver_path = (ARGV.length == 1 && ARGV[0]) || nil
-	Ferver.set :ferver_path, ferver_path
+    # use the first argument as the file path to serve from
+    ferver_path = (ARGV.length == 1 && ARGV[0]) || nil
+    Ferver.set :ferver_path, ferver_path
 
-	#  Run!
-	Ferver.run!
+    #  Run!
+    Ferver.run!
 
 end
 
 
 if __FILE__ == $0
-	main()
+    main()
 end
 
