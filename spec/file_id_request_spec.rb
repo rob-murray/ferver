996e3befc068a057f9bdc0058901afda2ac8be9d
Rob Murray <robmurray17@gmail.com>
2014-06-11 21:18:33 +0100

    Update spec syntax

--- spec/file_id_request_spec.rb
+++ spec/file_id_request_spec.rb
@@ -1,85 +1,65 @@
 require 'spec_helper'
 
 describe Ferver::FileIdRequest do
-
   describe 'creating new instance' do
-
     context 'when valid Integer is passed' do
-
-      let(:id_request) { Ferver::FileIdRequest.new(1) }
-
-      it 'should create instance' do
-        expect(id_request).not_to be_nil
-      end
+      subject { Ferver::FileIdRequest.new(1) }
 
       it 'should return expected value' do
-        expect(id_request.value).to eq(1)
+        expect(subject.value).to eq(1)
       end
-
     end
 
     context 'when nil argument is passed' do
-
       it 'should be invalid' do
         id_request = Ferver::FileIdRequest.new(nil)
-        expect(id_request.valid?).to be_false
+
+        expect(id_request.valid?).to be_falsey
       end
-
     end
-
   end
 
   describe '#value= method' do
-
-    let(:id_request) { Ferver::FileIdRequest.new }
+    subject { Ferver::FileIdRequest.new }
 
     context 'when valid Integer is passed' do
-
-      before { id_request.value = 1 }
+      before { subject.value = 1 }
 
       it 'should be valid' do
-        expect(id_request.valid?).to be_true
+        expect(subject.valid?).to be_truthy
       end
 
       it 'should return expected value' do
-        expect(id_request.value).to eq(1)
+        expect(subject.value).to eq(1)
       end
-
     end
 
     context 'when valid String as Integer is passed' do
-
-      before { id_request.value = '1' }
+      before { subject.value = '1' }
 
       it 'should be valid' do
-        expect(id_request.valid?).to be_true
+        expect(subject.valid?).to be_truthy
       end
 
       it 'should return expected value' do
-        expect(id_request.value).to eq(1)
+        expect(subject.value).to eq(1)
       end
-
     end
 
     context 'when a string is passed' do
-
-      before { id_request.value = 'foo' }
+      before { subject.value = 'foo' }
 
       it 'should be invalid' do
-        expect(id_request.valid?).to be_false
+        expect(subject.valid?).to be_falsey
       end
-
     end
 
     context 'when an empty string is passed' do
-
       it 'should be invalid' do
-        id_request.value = ''
-        expect(id_request.valid?).to be_false
+        subject.value = ''
+
+        expect(subject.valid?).to be_falsey
       end
-
     end
-
   end
-
 end
