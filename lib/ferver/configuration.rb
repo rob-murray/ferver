da6d3d564ceb0052612484fede15f462e0be49ff
Dan Milne <d@nmilne.com>
2015-08-24 13:34:55 +1000

    Add a configuration item to display / hide dot files.

--- lib/ferver/configuration.rb
+++ lib/ferver/configuration.rb
@@ -1,11 +1,17 @@
 module Ferver
   class Configuration
-    attr_accessor :directory_path
+    attr_accessor :directory_path, :serve_invisible
 
     # Return the absolute path to the directory Ferver is serving files from.
     #
     def directory_path
       @directory_path || Ferver::DEFAULT_FILE_SERVER_DIR_PATH
+    end
+
+    # Default to not serving invisible files
+    #
+    def serve_invisible
+      @serve_invisible || false
     end
   end
 end
