8f1febf0ace1e82776105de31b280fd961c49e55
Dan Milne <d@nmilne.com>
2015-08-24 13:58:19 +1000

    Add an etag attribute, then set it during initialisation, if it's a valid file.

--- lib/ferver/found_file.rb
+++ lib/ferver/found_file.rb
@@ -1,12 +1,13 @@
 module Ferver
   class FoundFile
-    attr_reader :file_name, :path_to_file
+    attr_reader :file_name, :path_to_file, :etag
 
     alias_method :name, :file_name
 
     def initialize(directory, file_name)
       @file_name = file_name
       @path_to_file = File.join(directory, file_name)
+      @etag = Digest::SHA1.hexdigest( File.read(@path_to_file ) ) if valid?
     end
 
     def valid?
